989dc3e7619ae942c4e3ae6f42e44333842e2cbc
Ally <allyy.noel@gmail.com>
2024-02-04 12:31:28 -0500

    shortcode-instagram1

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("user", function(name, instagramUsername) {
+    return `div class="user">
+  <div class="user_name">${name}>/div>
+  <div class="user_instagram".@${instagramUsername}</div>
+  </div>`
+  })
 };
